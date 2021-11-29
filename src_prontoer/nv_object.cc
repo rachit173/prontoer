030a7c410ff32f7cccb5c442b75e2b164aea7db2
Rachit Tibrewal <rachittibrewal@outlook.com>
2021-11-29 02:16:45 +0000

    Remove dependencies and files for persister.h and thread.h.

--- src_prontoer/nv_object.cc
+++ src_prontoer/nv_object.cc
@@ -4,7 +4,6 @@
 #include <queue>
 #include "nv_object.h"
 #include "nv_log.h"
-#include "thread.h"
 #include "recovery_context.h"
 
 /*
--- src_prontoer/persister.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void *Savitar_persister_worker(void *);
