336f405eecde26f73c5bea4b5376416b37aaec9d
Rachit Tibrewal <rachittibrewal@outlook.com>
2021-12-02 20:29:05 +0000

    Add thread specific workloads.

--- src_prontoer/main.cc
+++ src_prontoer/main.cc
@@ -25,19 +25,61 @@
 #include "persistent_ordered_set.h"
 using namespace std;
 
-void workerFunction(PersistentOrderedSet* obj) {
-  function<void()> start_routine = [&obj](){
-    // TODO: replace with benchmark code.
-    obj->insert(42);
-    auto ret = obj->get(42); 
-    if (ret != nullopt) {
-      cout << "Got: " << *ret << endl; 
-    } else {
-      cout << "Did not find key" << endl;
+enum Op { Insert, Remove, Get };
+struct Workload {
+  typedef uint64_t KeyType;
+  private:
+    vector<pair<Op, KeyType>> ops;
+    size_t i;
+  public:
+    Workload(): i(0) {}
+    void addOperation(Op op, KeyType key) {
+      ops.push_back({op, key});
+    }
+    void start() {
+      i = 0;
+    }
+    bool hasNext() {
+      return i < ops.size(); 
+    }
+    pair<Op, KeyType> next() {
+      return ops[i++];
+    }
+};
+
+void workerFunction(PersistentOrderedSet* obj, Workload workload) {
+  function<void()> start_routine = [&obj, &workload](){
+    workload.start();
+    while (workload.hasNext()) {
+      auto op = workload.next();
+      auto key = op.second;
+      switch (op.first)
+      {
+      case Insert:
+        obj->insert(key);
+        break;
+      case Remove:
+        obj->erase(key);
+        break;
+      case Get:
+        {
+          auto ret = obj->get(key);
+          if (ret != nullopt) {
+            cout << "Got: " << *ret << "\n";  
+          } else {
+            cout << "Did not find key\n";
+          }
+        }
+      default:
+        /* op not found */
+        break;
+      }
     }
   };
   initializeWorker(start_routine, (PersistentObject*)obj);
 }
+
+
 
 int main(int argc, char* argv[]) {
   coreInit();
@@ -46,10 +88,19 @@ int main(int argc, char* argv[]) {
   PersistentOrderedSet* pom = PersistentOrderedSet::Factory(pom_id);
   int num_workers = 2;
   vector<thread> workers;
-  for (int i = 0; i < num_workers-1; i++) {
-    workers.push_back(thread(workerFunction, pom));
+  Workload work[num_workers];
+  // Generate workloads for all threads.
+  for (int i = 0; i < num_workers; i++) {
+    work[i].addOperation(Insert, 42);
+    work[i].addOperation(Get, 42);
   }
-  workerFunction(pom);
+
+  // Run workload on the different threads with
+  // the common data structure `pom`.
+  for (int i = 0; i < num_workers-1; i++) {
+    workers.push_back(thread(workerFunction, pom, work[i]));
+  }
+  workerFunction(pom, work[num_workers-1]);
 
   for (auto& worker: workers) {
     worker.join();
